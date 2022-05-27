753420309aedb4e3181ba4beb19d1af8ce71cc98
Anna Pearsall <annakpearsall@gmail.com>
2022-05-26 17:07:29 -0700

    add date element to copyright in footer

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log('Hello World');
+console.log('Hi, welcome to my portfolio!');
+
+const cYear = document.querySelector('.year');
+const dateNow = new Date();
+const gotYear = dateNow.getFullYear();
+
+cYear.innerText = gotYear;
