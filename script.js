3eb322f58c77537415c46749b1b4572c675f3c1b
Anna Pearsall <annakpearsall@gmail.com>
2022-05-30 15:20:55 -0700

    add back to top button and scrollbar
    https://www.freecodecamp.org/news/back-to-top-button-and-page-progressbar-with-html-css-and-js/
    such a great tutorial!

--- script.js
+++ script.js
@@ -5,3 +5,42 @@ const dateNow = new Date();
 const gotYear = dateNow.getFullYear();
 
 cYear.innerText = gotYear;
+
+const showOnPx = 175;
+const backToTopButton = document.querySelector(".back-to-top")
+
+const scrollContainer = () => {
+  return document.documentElement || document.body;
+};
+
+document.addEventListener("scroll", () => {
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden")
+  } else {
+    backToTopButton.classList.add("hidden")
+  }
+})
+
+const goToTop = () => {
+    document.body.scrollIntoView({
+        behavior: 'smooth',
+    });
+  };
+
+  backToTopButton.addEventListener("click", goToTop)
+
+  const pageProgressBar = document.querySelector(".progress-bar")
+document.addEventListener("scroll", () => {
+  const scrolledPercentage =
+      (scrollContainer().scrollTop /
+        (scrollContainer().scrollHeight - scrollContainer().clientHeight)) *
+      100;
+  
+  pageProgressBar.style.width = `${scrolledPercentage}%`
+  
+  if (scrollContainer().scrollTop > showOnPx) {
+    backToTopButton.classList.remove("hidden");
+  } else {
+    backToTopButton.classList.add("hidden");
+  }
+});
